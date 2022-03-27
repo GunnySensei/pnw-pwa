55202de96c137c9832bfae0fdaced6c5e78a6fd5
GunnySensei <gunnerwagoner@hotmail.com>
2022-03-26 21:52:33 -0500

    moved file structure to be in public, fixed errors with offline functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,32 +2,71 @@
 let db;
 
 //establish connection to IndexedDB database
-const request = indexedDB.open('pnw-pwa', 1);
+const request = indexedDB.open("pnw-pwa", 1);
 
 //event will emit if the db version changes
-request.onupgradeneeded = function(event) {
-    //save ref to db
-    const db = event.target.result;
-    //create object store
-    db.createObjectStore('new_calculations')
-}
-
-request.onsuccess = function(event) {
-    db = event.target.result;
-
-    if(navigator.onLine) {
-        //uploadPizza();
-    }
+request.onupgradeneeded = function (event) {
+  //save ref to db
+  const db = event.target.result;
+  //create object store
+  db.createObjectStore("new_transaction", { autoIncrement: true });
 };
 
-request.onerror = function(event) {
-    console.log(event.target.errorCode);
-}
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    uploadtransaction();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
 
 function saveRecord(record) {
-    const transaction = db.transaction(['new_calculations'], 'readwrite');
+  const transaction = db.transaction(["new_transaction"], "readwrite");
 
-    const pizzaObjectStore = transaction.objectStore('new_calculations');
+  const transactionObjectStore = transaction.objectStore("new_transaction");
 
-    pizzaObjectStore.add(record);
+  transactionObjectStore.add(record);
 }
+
+function uploadtransaction() {
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  const transactionObjectStore = transaction.objectStore("new_transaction");
+
+  const getAll = transactionObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          //open another transaction
+          const transaction = db.transaction(["new_transaction", "readwrite"]);
+          //access new_transaction object store
+          const transactionObjectStore =
+            transaction.objectStore("new_transaction");
+          //clear items in store
+          transactionObjectStore.clear();
+
+          alert("All saved transaction have been submitted!");
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+}
+
+window.addEventListener("online", uploadtransaction);
